0d88344c3af5a5cae310209c663d193f35883aa8
Luis Moscoso <Lm239292@gmail.com>
2019-10-01 22:04:32 -0400

    t

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .set('views', path.join(__dirname, 'server'))
+  .set('view engine', 'html')
+  .get('/', (req, res) => res.render('static/myexample'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
